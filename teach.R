bbb20da95aee756c9a1ff88cf092d7c1e29d86fb
Didier Murillo <damurillof@gmail.com>
2018-08-13 10:15:49 -0400

    Go!

---
+++ teach.R
@@ -0,0 +1,16 @@
+#### 1.   [Bono Examen (NO obligatorio)](https://didier-murillo.netlify.com/Bono_Examen/)
+
+
+#> Tareas
+
+
+#### 1.   [Tarea 1](https://didier-murillo.netlify.com/Tarea1/)
+
+#### 2.   [Tarea 2](https://didier-murillo.netlify.com/tarea2/)
+
+#> Laboratorios 
+
+#### 1.   [Laboratorio 1](https://didier-murillo.netlify.com/Lab1/)
+
+
+#### 2.   [Laboratorio 2](https://didier-murillo.netlify.com/Lab2/)
